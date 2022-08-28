610783a48dc1c7e6f57c63af18dd1baf50a24390
jwcub <jwcub@outlook.com>
2022-08-28 18:08:53 +0800

    化繁为简

--- bot.js
+++ bot.js
@@ -3,10 +3,9 @@ let config = require("./config");
 
 let cookie = config.cookie;
 
-let myColor = -1;
+let myColor;
 let size = 20;
-let gm = [];
-let hasmap = false;
+let gm;
 
 let s = io("https://kana.byha.top:444/", {
     path: "/ws/checkmate",
@@ -15,7 +14,7 @@ let s = io("https://kana.byha.top:444/", {
 
 s.on("connect", () => {
     console.log("WS connected Successfully");
-    join_room();
+    joinRoom();
 });
 
 s.on("error", () => console.error("WS Error!"));
@@ -24,7 +23,7 @@ s.on("disconnect", () => console.warn("WS disconnected!"));
 
 s.on("reconnect", () => console.warn("WS reconnect!"));
 
-function join_room() {
+function joinRoom() {
     s.emit("joinRoom", config.gameRoom);
     console.log("join room", config.gameRoom);
     voteStart(1);
@@ -35,7 +34,6 @@ function voteStart(i) {
 }
 
 s.on("UpdateGM", (dat) => {
-    hasmap = true;
     size = dat[0].length - 1;
     gm = dat;
 });
@@ -45,9 +43,7 @@ s.on("UpdateColor", (dat) => myColor = dat);
 s.on("UpdateSize", (dat) => size = dat);
 
 s.on("Map_Update", (dat) => {
-    if (!hasmap) {
-        s.emit("Ask_GM");
-        hasmap = true;
+    if (!myColor || !gm) {
         return;
     }
 
@@ -59,7 +55,6 @@ s.on("Map_Update", (dat) => {
 });
 
 s.on("WinAnction", () => {
-    hasmap = false;
     target = [];
     voteStart(1);
     voteMap(1);
@@ -85,18 +80,9 @@ s.on("UpdateSettings", (dat) => {
         setSpeed(config.speed);
 });
 
-let dir = [
-    [-1, 0],
-    [0, 1],
-    [1, 0],
-    [0, -1]
-];
+let dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 let target = [];
 let from = [];
-
-function dist(p1, p2) {
-    return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
-}
 
 function getNeighbours(a) {
     let tmp = [];
@@ -124,7 +110,7 @@ function visible(p) {
     return false;
 }
 
-function move_to(sp, ep) {
+function moveTo(sp, ep) {
     let s = gm[sp[0]][sp[1]];
     let e = gm[ep[0]][ep[1]];
     let halfTag = 0;
@@ -152,10 +138,6 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randChoice(x) {
-    return x[randInt(0, x.length - 1)];
-}
-
 function shuffle(a) {
     let len = a.length;
 
@@ -168,7 +150,7 @@ function shuffle(a) {
     }
 }
 
-function updateMovement(tmp) {
+function uploadMovement(tmp) {
     if (tmp.length == 0) {
         return;
     }
@@ -177,7 +159,7 @@ function updateMovement(tmp) {
 }
 
 function changeTarget() {
-    let score = { 1: 1, 3: 1, 2: 1, 5: 2, 0: 2 };
+    let score = { 1: 1, 3: 1, 2: 1, 0: 2, 5: 3 };
     let tmp = [];
 
     for (let i = 1; i <= size; i++) {
@@ -215,11 +197,7 @@ function nextMove() {
     }
 
     function getScore(node) {
-        if (node.color == myColor) {
-            return node.amount - 1;
-        } else {
-            return -node.amount - 1;
-        }
+        return node.color == myColor ? node.amount - 1 : -node.amount - 1;
     }
 
     let maxAns = [], maxScore = -999999, newFrom = [];
@@ -230,7 +208,7 @@ function nextMove() {
         vis[[i, j]] = true;
 
         while (q.length != 0) {
-            let first = q.splice(0, 1)[0];
+            let first = q.shift();
             let cur = first[0], amount = first[1], length = first[2], ans = first[3];
 
             if (cur[0] == target[0] && cur[1] == target[1]) {
@@ -265,7 +243,20 @@ function nextMove() {
         for (let i = 1; i <= size; i++) {
             for (let j = 1; j <= size; j++) {
                 if (gm[i][j].color == myColor && gm[i][j].amount > 1) {
-                    bfs(i, j);
+                    let flag = true;
+
+                    for (let nxt of getNeighbours([i, j])) {
+                        let node = gm[nxt[0]][nxt[1]];
+
+                        if (node.color != myColor && (node.type == 2 || node.type == 3)) {
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                    if (flag) {
+                        bfs(i, j);
+                    }
                 }
             }
         }
@@ -286,7 +277,7 @@ function nextMove() {
         from = newFrom;
     }
 
-    let ans = move_to(from, maxAns);
+    let ans = moveTo(from, maxAns);
     from = maxAns;
     return ans;
 }
@@ -322,12 +313,12 @@ function expand() {
         return score[nodeA.type] - score[nodeB.type];
     });
 
-    return move_to(tmp[0][0], tmp[0][1]);
+    return moveTo(tmp[0][0], tmp[0][1]);
 }
 
 function botMove() {
     try {
-        updateMovement(randInt(1, 100) >= 70 ? nextMove() : expand());
+        uploadMovement(randInt(1, 100) >= 70 ? nextMove() : expand());
     } catch (error) {
         console.error(error);
     }
